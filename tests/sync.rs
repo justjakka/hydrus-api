f76c055ed65f71a7b4f778bacb2b75ddd52f53e3
jakka <jakka@jakka.su>
2025-10-17 17:12:36 +0300

    removed boilerplate

--- tests/sync.rs
+++ tests/sync.rs
@@ -2,21 +2,23 @@ use hydrus_api_rs::sync_lib::client::HydrusClient;
 use hydrus_api_rs::sync_lib::traits::*;
 use std::path::PathBuf;
 
-#[test]
-fn test_service_name_info() {
+fn init_client() -> HydrusClient {
     let keyfile = PathBuf::from("secrets");
     let key = std::fs::read_to_string(keyfile).unwrap().trim().to_string();
     let mut client: HydrusClient = HydrusClient::new("http://127.0.0.1:45869/");
     client.set_api_key(key);
+    client
+}
+
+#[test]
+fn test_service_name_info() {
+    let client = init_client();
     let _ = client.get_service_name("all my files").unwrap();
 }
 
 #[test]
 fn test_service_key_info() {
-    let keyfile = PathBuf::from("secrets");
-    let mut client: HydrusClient = HydrusClient::new("http://127.0.0.1:45869/");
-    let key = std::fs::read_to_string(keyfile).unwrap().trim().to_string();
-    client.set_api_key(key);
+    let client = init_client();
     let _ = client
         .get_service_key("616c6c206c6f63616c206d65646961")
         .unwrap();
@@ -24,10 +26,7 @@ fn test_service_key_info() {
 
 #[test]
 fn test_get_services() {
-    let mut client: HydrusClient = HydrusClient::new("http://127.0.0.1:45869/");
-    let keyfile = PathBuf::from("secrets");
-    let key = std::fs::read_to_string(keyfile).unwrap().trim().to_string();
-    client.set_api_key(key);
+    let client = init_client();
     let res = client.get_services().unwrap();
     assert!(!res.is_empty())
 }
